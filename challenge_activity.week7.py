17701af54fe6d7336c3e60074f7c2913b85b7c26
Munoz <mimunoz3@cps.k12.il.us>
2024-10-07 09:32:40 -0500

    first commit

---
+++ challenge_activity.week7.py
@@ -0,0 +1,40 @@
+question = input ("Enter a text: ")
+print(question.lower())
+
+
+
+
+# Asks what the three letters are
+letter1 = input("Enter the first letter: ")
+
+letter2 = input("Enter the second letter: ")
+
+letter3 = input("Enter the third letter: ")
+
+
+
+# converts the three letters into a list
+letters_list = [letter1, letter2, letter3]
+print(letters_list).lower())
+
+
+
+
+
+
+# how many times those letters appear, then print
+# how many words are in the entire text (tranform into list)
+# find first and last letter of the 
+
+
+
+
+# see if "Python" is inside text using a boolean
+
+
+
+
+
+
+
+
